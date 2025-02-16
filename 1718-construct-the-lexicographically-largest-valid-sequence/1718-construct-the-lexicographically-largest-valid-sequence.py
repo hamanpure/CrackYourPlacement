031aa3ab7154a6bcafd98a8a765e439de48450d1
HAMANPURE VAIBHAV <125445741+hamanpure@users.noreply.github.com>
2025-02-16 13:50:26 +0530

    Time: 4 ms (68.75%), Space: 17.9 MB (41.41%) - LeetHub

--- 1718-construct-the-lexicographically-largest-valid-sequence/1718-construct-the-lexicographically-largest-valid-sequence.py
+++ 1718-construct-the-lexicographically-largest-valid-sequence/1718-construct-the-lexicographically-largest-valid-sequence.py
@@ -2,9 +2,9 @@ class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
         res = [0]*(2*n-1)
         used = set()
-        len_res = 2*n-1
+
         def backtrack(i):
-            if i == len_res: 
+            if i == len(res): 
                 return True
 
             #Try largest number
@@ -12,7 +12,7 @@ class Solution:
                 #Validation
                 if num in used:
                     continue
-                if num > 1 and (i + num >= len_res) or res[i+num]:
+                if num > 1 and ((i + num >= len(res)) or res[i+num]):
                     continue
                 
                 #Try Decision 
@@ -20,9 +20,11 @@ class Solution:
                 res[i] = num
                 if num>1:
                     res[i+num] = num
+
                 j = i+1
                 while j<len(res) and res[j]:
                     j += 1
+
                 #recursive step
                 if backtrack(j):
                     return True
