fef4512c6a451a7b8cf6f4edf70af8fe7c549ca8
HAMANPURE VAIBHAV <125445741+hamanpure@users.noreply.github.com>
2025-04-11 23:05:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        count = 0
+        for i in range(low, high+1):
+            s = str(i)
+            s1 = sum(int(d) for d in s[:len(s)//2])
+            s2 = sum(int(d) for d in s[len(s)//2:])
+            if s1==s2:
+                count+=1
+        return count
+
+
